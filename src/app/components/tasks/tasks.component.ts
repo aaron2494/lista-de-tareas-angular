b3c825e3d1b4f53b7f513843a2e0a5d050a7e37a
aaron <aaron.e.francolino@gmail.com>
2022-03-17 20:06:47 -0300

    componenente lista de items

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -8,6 +9,7 @@ import { Task } from 'src/app/Task';
 })
 export class TasksComponent implements OnInit {
   tasks:Task[]=TASKS;
+  
   constructor() { }
 
   ngOnInit(): void {
